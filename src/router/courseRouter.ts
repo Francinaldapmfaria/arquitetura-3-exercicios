123836eb4916425f9a85b2dc9b2d91b8849759d4
francinaldapmfaria <portela854@gmail.com>
2023-02-08 14:49:23 -0300

    inicio-exercicio

--- src/router/courseRouter.ts
+++ src/router/courseRouter.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import { CourseBusiness } from "../business/CourseBusiness"
 import { CourseController } from "../controller/CourseController"
 
 export const courseRouter = express.Router()
 
-const courseController = new CourseController()
+const courseController = new CourseController(
+    new CourseBusiness()
+)
 
 courseRouter.get("/", courseController.getCourses)
 courseRouter.post("/", courseController.createCourse)
